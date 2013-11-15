98e3ee8b73c927e78631eefcf508031a1d2f2ecd
JohnReid <johnbaronreid@netscape.net>
2013-11-15 10:12:58 +0000

    Add git Id attribute

--- python/seqan/__init__.py
+++ python/seqan/__init__.py
@@ -8,7 +8,7 @@ import pkg_resources
 
 __doc__ = pkg_resources.resource_string(__name__, "README")
 __license__ = pkg_resources.resource_string(__name__, "LICENSE")
-__release__, __svn_revision__ = pkg_resources.resource_string(__name__, "VERSION").strip().split('-')
+__release__, __git_id__ = pkg_resources.resource_string(__name__, "VERSION").strip().split('-')
 __major_version__, __minor_version__, __release_version__ = map(int, __release__.split('.'))
 __version__ = '%d.%d' % (__major_version__, __minor_version__)
 
@@ -23,7 +23,7 @@ except AttributeError:
 
 
 def version_string():
-    """Return the release and svn revision as a string."""
-    return '%s %s' % (__release__, __svn_revision__)
+    """Return the release and git id as a string."""
+    return '%s %s' % (__release__, __git_id__)
 
 
